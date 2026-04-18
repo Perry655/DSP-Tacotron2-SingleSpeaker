9a11c5dc6cdd2b81c5b2ad3eef472ee8ff1d3811
Perry655 <97612695+Perry655@users.noreply.github.com>
2026-04-18 21:37:24 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -148,7 +148,8 @@ def load_and_setup_model(model_name, parser, checkpoint, fp16_run, cpu_run,
         if cpu_run:
             state_dict = torch.load(checkpoint, map_location=torch.device('cpu'))['state_dict']
         else:
-            state_dict = torch.load(checkpoint)['state_dict']
+            #state_dict = torch.load(checkpoint)['state_dict']
+            state_dict = torch.load(checkpoint, weights_only=False)['state_dict']
         if checkpoint_from_distributed(state_dict):
             state_dict = unwrap_distributed(state_dict)
 
